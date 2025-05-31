8e17bf0f432ac0275b2b67699ea96f16d559f933
Tech-sis123 <esabublessing7@gmail.com>
2025-05-31 13:00:27 +0100

    Change in second commit

--- models/verification.js
+++ models/verification.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.MONGO_URI)
 
 const verificationSchema = new mongoose.Schema({
   inputText: { type: String, required: true },
